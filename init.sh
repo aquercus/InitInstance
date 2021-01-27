bf7824a0de2d6b3cde27a571d64670d1fd5a38b8
Helen <aquercus@gmail.com>
2021-01-26 21:54:40 -0600

    -y option added

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
+set -e
 
 yum update -y
 
 yum install java-1.8.0-openjdk.x86_64 -y
 java -version
 
-amazon-linux-extras install docker
+amazon-linux-extras install docker  -y
 service docker start
 docker ps
 
